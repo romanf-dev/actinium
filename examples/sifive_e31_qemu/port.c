d1a12c63ebbd676fcc49b621bc692fec0bfe2f81
romanf <>
2026-01-30 23:01:43 +0300

    Fix: minor updates for SiFive E31 example

--- examples/sifive_e31_qemu/port.c
+++ examples/sifive_e31_qemu/port.c
@@ -8,10 +8,45 @@
 #include "actinium.h"
 #include "mtimer.h"
 
+#define AC_GPIC_CLZ(v) mg_port_clz(v)
+#define AC_GPIC_REQUEST(bit) (*((volatile unsigned*) 0x2000000) = bit)
+#include "arch/test/ac_gpic.h"
+
 #define MCAUSE_ENVCALL 8
 
+//
+// E31 doesn't support CLZ, so implement it manually.
+//
+unsigned int mg_port_clz(uint32_t v) {
+    uint32_t r = !v;
+    uint32_t c = (v < 0x00010000u) << 4;
+    r += c;
+    v <<= c;
+    c = (v < 0x01000000u) << 3;
+    r += c;
+    v <<= c;
+    c = (v < 0x10000000u) << 2;
+    r += c;
+    v <<= c;
+    c = (v >> 27) & 0x1e;
+    r += (0x55afu >> c) & 3;
+    return r;
+}
+
+//
 // sw-implemented fake 'interrupt controller' simulated in MS interrupt.
-struct ac_gpic_t g_pic;
+//
+static struct ac_gpic_t g_pic;
+
+unsigned pic_vect2prio(unsigned vect) {
+    return vect;
+}
+
+void pic_interrupt_request(unsigned cpu, unsigned vect) {
+    mg_critical_section_enter();
+    ac_gpic_request(&g_pic, vect);
+    mg_critical_section_leave();    
+}
 
 // This port does not handle hardware interrupts except mtimer/msoftirq.
 struct ac_port_frame_t* ac_port_mei_handler(struct ac_port_frame_t* frame) {
